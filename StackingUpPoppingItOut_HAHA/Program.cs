a77df19cdae2d42be283761ecdc1e5d6b5e35a62
Andrew Germann <AlwaysTinkering@outlook.com>
2015-09-14 22:29:28 +0000

    Played with Stacks and Queues..... :D

---
+++ StackingUpPoppingItOut_HAHA/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackingUpPoppingItOut_HAHA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            while (stack.Count > 0)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+
+            Console.ReadLine();
+
+            
+        }
+    }
+}
